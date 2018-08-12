e683d3bbe5d5b8241d35d79b012cd08ae0c40221
mka <mka281@icloud.com>
2018-08-11 23:02:41 -0700

    Define GET block route

--- api/block.js
+++ api/block.js
@@ -1,17 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const Blockchain = require("../blockchain/Blockchain");
 
-// @route   GET api/block/test
-// @desc    Tests block route
+// @route   GET /block/:blockHeight
+// @desc    Retrieve block from database
 // @access  Public
-router.get("/:blockHeight", (req, res) =>
-  res.json({ block: `Block ${blockHeight}` })
-);
+router.get("/:blockHeight", (req, res) => {
+  Blockchain.getBlock(req.params.blockHeight)
+    .then(block => {
+      res.json(block);
+    })
+    .catch(err =>
+      res.json({ NotFoundError: `Key ${blockHeight} not found in database` })
+    );
+});
 
-// @route   POST api/users/login
-// @desc    Login user / Returning JWT
+// @route   POST /block
+// @desc    Tests block route
 // @access  Public
 router.post("/", (req, res) =>
   res.json({ message: "New block added to chain" })
 );
+
 module.exports = router;
